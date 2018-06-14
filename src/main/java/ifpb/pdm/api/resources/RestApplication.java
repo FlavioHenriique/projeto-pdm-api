a9edec9aac95c925a85065af15c78cf7d7c22e29
FlavioHenriique <flaviohenrique638@gmail.com>
2018-06-14 13:28:20 -0300

    Adicionando arquivos do projeto

---
+++ src/main/java/ifpb/pdm/api/resources/RestApplication.java
@@ -0,0 +1,10 @@
+package ifpb.pdm.api.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("pdm-api")
+public class RestApplication extends Application{
+}
