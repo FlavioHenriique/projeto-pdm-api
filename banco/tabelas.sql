9e8fdb774e66cbf71b08a84bdc00262d341c9011
FlavioHenriique <flaviohenrique638@gmail.com>
2018-06-14 13:47:46 -0300

    Criando pasta para informações sobre o banco de dados

---
+++ banco/tabelas.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Usuario(
+	nome VARCHAR,
+	email VARCHAR PRIMARY KEY,
+	senha VARCHAR,
+	estado VARCHAR
+);
+
+CREATE TABLE Trabalho(
+	titulo VARCHAR,
+	estado VARCHAR,
+	cidade VARCHAR,
+	valor real,
+	horario timestamp,
+	data date,
+	codigo SERIAL PRIMARY KEY,
+	descricao VARCHAR,
+	contratante VARCHAR,
+	FOREIGN KEY (contratante) REFERENCES Usuario(email)
+);
+
+CREATE TABLE SOLICITA_TRABALHO(
+	emailUsuario VARCHAR,
+	codTrabalho int,
+	FOREIGN KEY (emailUsuario) REFERENCES Usuario(email),
+	FOREIGN KEY (codTrabalho) REFERENCES Trabalho(codigo),
+	PRIMARY KEY (emailUsuario,codTrabalho)
+);
